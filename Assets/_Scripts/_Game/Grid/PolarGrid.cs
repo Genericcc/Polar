3eb6f3bf59fcc53c81c31ef03a3dda0c96ec4f13
Bolan <m.bolan.m@gmail.com>
2026-08-22 17:56:41 +0200

    Passing settings while creating grid

--- Assets/_Scripts/_Game/Grid/PolarGrid.cs
+++ Assets/_Scripts/_Game/Grid/PolarGrid.cs
@@ -31,28 +31,27 @@ namespace _Scripts._Game.Grid
 
             for (var ringIndex = 0; ringIndex < _polarGridRingsSettings.ringSettingsList.Count; ringIndex++)
             {
-                var ring = ringFactory.Create(ringIndex);
-                
+                var ring = ringFactory.Create(ringIndex, _polarGridRingsSettings.ringSettingsList[ringIndex]);
+
                 var startDistanceToWorldOrigin = endDistanceToWorldOrigin;
                 endDistanceToWorldOrigin += ring.RingSettings.depth * _columnHeight;
-                
                 ring.SetBounds((startDistanceToWorldOrigin, endDistanceToWorldOrigin));
-                ring.CreateMesh(50, ring.RingSettings.material);
-                ring.PopulateWithNodes(polarNodeFactory);
                 
-                GridNodes.AddRange(ring.Nodes);
+                ring.CreateMesh(50, ring.RingSettings.material);
                 
                 Rings.Add(ring);
+                ring.PopulateWithNodes(polarNodeFactory);
+                GridNodes.AddRange(ring.Nodes);
             }
         }
 
         public Vector3 GetWorldFromPolar(PolarGridPosition polarGridPosition)
         {
-            var previousFieldsCount = GetSumOfPreviousFields(polarGridPosition.ParentRingIndex);
-            
-            var x = (polarGridPosition.D + previousFieldsCount) * _columnHeight * Mathf.Cos(-polarGridPosition.Fi * Mathf.Deg2Rad);
+            var radius = Rings[polarGridPosition.ParentRingIndex].Bounds.min + polarGridPosition.D * _columnHeight;
+
+            var x = radius * Mathf.Cos(-polarGridPosition.Fi * Mathf.Deg2Rad);
             var y = polarGridPosition.H;
-            var z = (polarGridPosition.D + previousFieldsCount) * _columnHeight * Mathf.Sin(-polarGridPosition.Fi * Mathf.Deg2Rad);
+            var z = radius * Mathf.Sin(-polarGridPosition.Fi * Mathf.Deg2Rad);
 
             return new Vector3(x, y, z);
         }
@@ -64,26 +63,6 @@ namespace _Scripts._Game.Grid
             var z = purePolar.Radius * Mathf.Sin(-purePolar.Fi * Mathf.Deg2Rad);
 
             return new Vector3(x, y, z);
-        }
-
-        private int GetSumOfPreviousFields(int ringIndex)
-        {
-            var fields = 0;
-
-            if (ringIndex == 0)
-            {
-                return 0;
-            }
-            
-            for (var r = 1; r < _polarGridRingsSettings.ringSettingsList.Count; r++)
-            {
-                if (r <= ringIndex)
-                {
-                    fields += _polarGridRingsSettings.ringSettingsList[r - 1].depth;
-                }
-            }
-            
-            return fields;
         }
 
         public bool TryGetNodesForBuilding(PolarNode originNode, (int side, int depth) shift, out List<PolarNode> nodesForBuilding)
--- Assets/_Scripts/_Game/Grid/RingFactory.cs
+++ Assets/_Scripts/_Game/Grid/RingFactory.cs
@@ -6,32 +6,28 @@ using Zenject;
 
 namespace _Scripts._Game.Grid
 {
-    public class RingFactory : PlaceholderFactory<int, Ring>
+    public class RingFactory : PlaceholderFactory<int, RingSettings, Ring>
     {
     }
-    
-    public class CustomRingFactory : IFactory<int, Ring>
+
+    public class CustomRingFactory : IFactory<int, RingSettings, Ring>
     {
         private readonly DiContainer _container;
-        private readonly PolarGridRingsSettings _polarGridRingsSettings;
         private readonly PolarGridManager _polarGridManager;
         private readonly Ring _ringPrefab;
 
         public CustomRingFactory(DiContainer container,
-            PolarGridRingsSettings polarGridRingsSettings,
             PolarGridManager polarGridManager,
             Ring ringPrefab)
         {
             _container = container;
-            _polarGridRingsSettings = polarGridRingsSettings;
             _polarGridManager = polarGridManager;
             _ringPrefab = ringPrefab;
         }
-        
-        public Ring Create(int ringIndex)
+
+        public Ring Create(int ringIndex, RingSettings ringSettings)
         {
             var ring = _container.InstantiatePrefabForComponent<Ring>(_ringPrefab);
-            var ringSettings = _polarGridRingsSettings.ringSettingsList[ringIndex];
 
             ring.name = "Ring_" + ringIndex;
             ring.transform.SetParent(_polarGridManager.transform, true);
